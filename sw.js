a5ca7810224836f071e8d7eac6713ae660ad8f49
MonkeEvan <iamepical14@gmail.com>
2025-12-03 18:06:14 -0500

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-importScripts('https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet@3.2.10/dist/uv.bundle.js');
-
-const sw = new UVServiceWorker();
-
-self.addEventListener('fetch', (event) => event.respondWith(sw.fetch(event)));
